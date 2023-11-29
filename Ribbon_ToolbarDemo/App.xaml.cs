cdb73f639384be645519b978c5ca8f9c379f4e88
ArdaBs <134811694+ArdaBs@users.noreply.github.com>
2023-11-29 13:46:34 +0100

    Projektdateien hinzufügen.

---
+++ Ribbon_ToolbarDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ribbon_ToolbarDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
